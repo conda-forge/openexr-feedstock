3a27c3284b0c350b36efe58596b4f949c6194cc1
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-03-24 08:31:33 +0100

    make linter happy, use ninja

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,10 +9,10 @@ export CFLAGS="$CFLAGS -D__STDC_FORMAT_MACROS"
 export CXXFLAGS="$CXXFLAGS -D__STDC_FORMAT_MACROS -D_LIBCPP_DISABLE_AVAILABILITY"
 
 cmake \
+    -G Ninja \
     ${CMAKE_ARGS} \
     -DBUILD_SHARED_LIBS=ON \
     -DOPENEXR_LIB_SUFFIX="" \
     ..
 
-make -j${CPU_COUNT}
-make install
+ninja install
